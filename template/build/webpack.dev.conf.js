161d294aedf40f2189f37e96eb95dbc8c769bf6b
Eric Seigers <eric@gplusmedia.com>
2017-02-08 11:15:23 +0900

    pathing fix and scss added

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -9,6 +9,10 @@ module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
+  output: {
+    path: config.build.assetsRoot,
+    filename: utils.assetsPath('js/[name].js')
+  },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
